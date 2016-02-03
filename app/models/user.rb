a0f3922dae79ff3a8e23154209ecab446fc275f4
olegstasiv <oleg.stasiv@hotmail.com>
2016-02-03 16:08:31 +0200

    add fix to password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-  validates :password, length: { minimum: 6 }
-  validates_confirmation_of :password,
-                            if: lambda { |m| m.password.present? }
+  validates :password, length: { minimum: 6 }, unless: Proc.new { |user| user.password.nil? }
+  validates :password_confirmation, presence: true, unless: Proc.new { |user| user.password.nil? }
+  #validates_confirmation_of :password,
+                            #if: lambda { |m| m.password.present? }
 
   has_secure_password
 end
