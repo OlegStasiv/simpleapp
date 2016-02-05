e1b22013ccb41f0374e98448e04ea93367f193a3
biziliavv <biziliavv@gmail.com>
2016-02-05 12:03:24 +0200

    Friendship refferences are corrected

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204171955) do
+ActiveRecord::Schema.define(version: 20160205094917) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160204171955) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "friendships", ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
